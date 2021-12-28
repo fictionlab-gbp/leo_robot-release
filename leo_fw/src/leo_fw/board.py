0e00cbf1922bcfeab4ce696fc8c5bca9ec3d127f
Błażej Sowa <bsowa123@gmail.com>
2021-12-28 20:02:55 +0000

    Imported upstream version '2.0.1' of 'upstream'

--- leo_fw/src/leo_fw/board.py
+++ leo_fw/src/leo_fw/board.py
@@ -8,8 +8,8 @@ from std_srvs.srv import Trigger
 
 
 class BoardType(Enum):
+    LEOCORE = "leocore"
     CORE2 = "core2"
-    LEO_HAT = "leo_hat"
 
     def __str__(self):
         return self.value
@@ -18,16 +18,16 @@ class BoardType(Enum):
 def determine_board() -> Optional[BoardType]:
     services = rosservice.get_service_list()
 
-    if "/firmware/get_board_type" in services:
-        get_board_type = rospy.ServiceProxy("/firmware/get_board_type", Trigger)
+    if rospy.resolve_name("firmware/get_board_type") in services:
+        get_board_type = rospy.ServiceProxy("firmware/get_board_type", Trigger)
         type_str = get_board_type().message
-        if type_str == "core2":
+        if type_str == str(BoardType.CORE2):
             return BoardType.CORE2
-        elif type_str == "leo_hat":
-            return BoardType.LEO_HAT
+        elif type_str == str(BoardType.LEOCORE):
+            return BoardType.LEOCORE
 
     # Support legacy firmware
-    if "/core2/get_firmware_version" in services:
+    if rospy.resolve_name("core2/get_firmware_version") in services:
         return BoardType.CORE2
 
     return None
@@ -36,14 +36,14 @@ def determine_board() -> Optional[BoardType]:
 def check_firmware_version() -> str:
     services = rosservice.get_service_list()
 
-    if "/firmware/get_firmware_version" in services:
+    if rospy.resolve_name("firmware/get_firmware_version") in services:
         get_firmware_version = rospy.ServiceProxy(
-            "/firmware/get_firmware_version", Trigger
+            "firmware/get_firmware_version", Trigger
         )
     # Support legacy firmware
-    elif "/core2/get_firmware_version" in services:
+    elif rospy.resolve_name("core2/get_firmware_version") in services:
         get_firmware_version = rospy.ServiceProxy(
-            "/core2/get_firmware_version", Trigger
+            "core2/get_firmware_version", Trigger
         )
     else:
         return "<unknown>"
